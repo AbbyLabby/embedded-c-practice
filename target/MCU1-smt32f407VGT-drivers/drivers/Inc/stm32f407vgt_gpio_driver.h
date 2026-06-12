8561cfd9152492b6a822f2db44a817b5db072e05
AbbyLabby <savhenko14@gmail.com>
2026-06-13 01:25:44 +0300

    fixed issues

--- target/MCU1-smt32f407VGT-drivers/drivers/Inc/stm32f407vgt_gpio_driver.h
+++ target/MCU1-smt32f407VGT-drivers/drivers/Inc/stm32f407vgt_gpio_driver.h
@@ -109,6 +109,6 @@ void     GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, int16_t Value);      /* W
 void     GPIO_ToggleOutptPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);     /* Toggle the current output state of a single pin              */
 
 void     GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t Status);      /* Enable/disable an IRQ line in the NVIC (processor side)        */
-void     GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);  /* Set IRQ priority in the NVIC IPR registers (0 = highest)       */
+void     GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);  /* Set IRQ priority in the NVIC IPR registers (0 = highest)       */
 void     GPIO_IRQHandling(uint8_t PinNumber);                             /* Call from EXTIx_IRQHandler to clear the EXTI pending bit       */
 #endif /* INC_STM32F407VGT_GPIO_DRIVER_H_ */
--- target/MCU1-smt32f407VGT-drivers/drivers/Src/stm32f407vgt_gpio_driver.c
+++ target/MCU1-smt32f407VGT-drivers/drivers/Src/stm32f407vgt_gpio_driver.c
@@ -439,7 +439,7 @@ void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t Status)
  *                    hence the extra (8 - NO_PR_BITS_IMPLEMENTED) shift.
  *
  *********************************************************************************/
-void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
 {
 	// Find the IPR register and the byte section within it
 	uint8_t iprx = IRQNumber / 4;             // which IPR register
@@ -448,7 +448,7 @@ void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
 	// Lower bits of each byte are not implemented, so shift the priority up
 	uint8_t shift_amount = ( (8 * iprx_section) + (8 - NO_PR_BITS_IMPLEMENTED) );
 
-	*(NVIC_PR_BASE_ADDR + ( iprx * 4 )) |= ( IRQPriority << shift_amount );
+	*(NVIC_PR_BASE_ADDR + iprx) |= ( IRQPriority << shift_amount );
 }
 
 /*********************************************************************************
